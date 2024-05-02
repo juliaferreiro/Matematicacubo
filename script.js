3bde43751dbffe32dacc17600759b7dfdc550959
Julita <jvferreriro@escuelasproa.edu.ar>
2024-05-02 11:46:11 -0300

    Potencia de números imaginarios

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ function calcular() {
 
     document.getElementById("resultado").textContent = resultado;
 }
+
+function calcularpotencia(){
+
+    var exponente = parseInt(document.getElementById("exponente").value);
+    var resultadopotencia;
+
+    resultadopotencia = exponente % 4
+
+    if (resultadopotencia == 0){
+        document.getElementById("resultadopotencia").textContent = "El resultado es: 1"
+    }
+    if (resultadopotencia == 1){
+        document.getElementById("resultadopotencia").textContent = "El resultado es: i"
+    }
+    if (resultadopotencia == 2){
+        document.getElementById("resultadopotencia").textContent = "El resultado es: -1"
+    }
+    if (resultadopotencia == 3){
+        document.getElementById("resultadopotencia").textContent = "El resultado es -i"
+    }
+
+} 
